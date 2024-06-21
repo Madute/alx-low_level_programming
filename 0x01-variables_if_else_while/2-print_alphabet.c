5c97df4bcd106b734bf8f110516b8093a946c197
Madute Ledwaba <maduteledwaba@gmail.com>
2024-06-22 00:05:12 +0200

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,17 @@
-int main() {
-    char c;
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char c;
     for (c = 'a'; c <= 'z'; c++) {
         putchar(c);
     }
     putchar('\n');
     return 0;
 }
+
