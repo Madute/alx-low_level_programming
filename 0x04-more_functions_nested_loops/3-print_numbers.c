2539b5f369215aa098b25e980f507182ccfbb164
Madute Ledwaba <maduteledwaba@gmail.com>
2024-07-25 22:47:39 +0200

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-*print_most_numbers - prints numbers except 2 and 4
+*print_numbers - function that print all numbers from
+*0 to 9
 *
 *Return: returns nothing
 */
-void print_most_numbers(void)
+void print_numbers(void)
 {
-int number;
+int number = 48;
 for (number = 48; number < 58; number++)
 {
 _putchar(number);
