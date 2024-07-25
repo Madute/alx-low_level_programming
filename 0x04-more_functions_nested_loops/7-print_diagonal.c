34e61fabef6db5743aa00f761e16705690bad64f
Madute Ledwaba <maduteledwaba@gmail.com>
2024-07-25 22:24:05 +0200

    0x04

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * print_diagonal - this function print diagonals
+ * @n: larger of the digonal
+(* a blank line
+* Description: this function prints diagonals?
+(* section header: the header of this function is holberton.h)*
+* Return: a void function, no return
+*/
+void print_diagonal(int n)
+{
+int i, j, spaces;
+
+spaces = 0;
+
+
+n <= 0 ? _putchar('\n') : n;
+
+for (i = 0; i < n; i++)
+{
+if (i > 0)
+{
+for (j = 0; j < spaces; j++)
+{
+_putchar(' ');
+}
+}
+spaces++;
+if (n > 0)
+{
+_putchar('\\');
+}
+_putchar('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_square - this function print a square
+ * @size: larger of the square.
+(* a blank line
+* Description: this function prints a square?
+(* section header: the header of this function is holberton.h)*
+* Return: a void function, no return
+*/
+void print_square(int size)
+{
+int i, j;
+
+size <= 0 ? _putchar('\n') : size;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
